2586f4cd0325d441f73d8860fd9db7edf3a9bbe9
Yaheze <sukomel98@gmail.com>
2024-11-08 19:53:28 +0200

    i legit have no good ideas of how to continue, so that's that

--- index.js
+++ index.js
@@ -1,6 +1,42 @@
-let Menu = document.querySelector(".Menu")
-let Game = document.querySelector(".Game")
+let menu = document.querySelector(".Menu")
+let start = menu.querySelector(".StartButton")
+let create = menu.querySelector(".CreateButton")
+let game = document.querySelector(".Game")
+let question = game.querySelector(".QuestionField")
+let answers = game.querySelector(".Answers")
+let multAnswersProceed = game.querySelector(".MultipleAnswerProceed")
+let time = game.querySelector(".TimePassed")
+let results = document.querySelector(".GameResults")
+let resultsfield = results.querySelector(".ResultsField")
 
-let Test = fetch("./Files/example.json")
-
-console.log(Test)
+let placeholder = {
+    "name":"hi",
+    "contents":[
+        {
+            "question":"question 1",
+            "type":"quiz",
+            "answers":["hi","hello","none"],
+            "correctanswers":["none"],
+            "multiple":false
+        },
+        {
+            "question":"question 2: electric boogaloo",
+            "type":"quiz",
+            "answers":["this answer is correct","this answer is also correct","this answer is incorrect"],
+            "correctanswers":["this answer is correct","this answer is also correct"],
+            "multiple":false
+        },
+        {
+            "question":"select them all",
+            "type":"quiz",
+            "answers":["left","right"],
+            "correctanswers":["left","right"],
+            "multiple":true
+        },
+        {
+            "question":"mayo is an instrument",
+            "type":"condition",
+            "correctanswer":false
+        }
+    ]
+}
